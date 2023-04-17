e498d42468990bc9c7429679965248bd2a8dd3ac
Enzo Petry <54451768+EnzoPetry@users.noreply.github.com>
2023-04-17 20:51:03 -0300

    Segunda Activity

---
+++ app/src/main/java/com/example/app_desenvolvimento_android/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.app_desenvolvimento_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botaoTela = findViewById(R.id.trocar_activity);
+        botaoTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Tela2.class);
+                startActivity(intent);
+            }
+        });
+
+        Button botaoConfig = findViewById(R.id.configuracoes);
+        botaoConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implemente a lógica para abrir a Activity de configurações aqui
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/app_desenvolvimento_android/Tela2.java
@@ -0,0 +1,14 @@
+package com.example.app_desenvolvimento_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela2);
+    }
+}
