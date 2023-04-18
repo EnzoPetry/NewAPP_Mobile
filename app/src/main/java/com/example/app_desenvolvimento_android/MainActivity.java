643c674a5dbfb34f2b2d35ea6277e85ebb5365ae
Enzo Petry <54451768+EnzoPetry@users.noreply.github.com>
2023-04-17 21:06:46 -0300

    Removendo ActionBar

--- app/src/main/java/com/example/app_desenvolvimento_android/MainActivity.java
+++ app/src/main/java/com/example/app_desenvolvimento_android/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button botaoTela = findViewById(R.id.trocar_activity);
         botaoTela.setOnClickListener(new View.OnClickListener() {
