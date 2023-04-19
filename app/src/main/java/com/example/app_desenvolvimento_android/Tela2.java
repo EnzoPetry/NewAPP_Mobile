876be518ef86820fd214390dd3dd7ed795eab8f7
Enzo Petry <54451768+EnzoPetry@users.noreply.github.com>
2023-04-19 00:18:13 -0300

    Funcionalidade do botao e ScrollView

--- app/src/main/java/com/example/app_desenvolvimento_android/Tela2.java
+++ app/src/main/java/com/example/app_desenvolvimento_android/Tela2.java
@@ -3,6 +3,9 @@ package com.example.app_desenvolvimento_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.List;
@@ -15,15 +18,27 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Tela2 extends AppCompatActivity {
     private TextView textView;
+    private EditText idUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela2);
 
         textView = findViewById(R.id.textView);
-        getUsuarios();
+        idUsuario = findViewById(R.id.idUsuario);
+
+        Button buscaInfo = findViewById(R.id.buscaInfo);
+        buscaInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implemente a lógica para abrir a Activity de configurações aqui
+                getUsuarios();
+            }
+
+        });
     }
-    private void getUsuarios(){
+    private void getUsuarios() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -34,16 +49,14 @@ public class Tela2 extends AppCompatActivity {
         call.enqueue(new Callback<List<Usuarios>>() {
             @Override
             public void onResponse(Call<List<Usuarios>> call, Response<List<Usuarios>> response) {
-                if(!response.isSuccessful()){
+                if (!response.isSuccessful()) {
                     textView.setText("codigo: " + response.code());
                     return;
                 }
                 List<Usuarios> listaUsuarios = response.body();
-                for(Usuarios usuario: listaUsuarios){
+                for (Usuarios usuario : listaUsuarios) {
                     String content = "";
-                    content += "Id:" + usuario.getId() + "\n";
-                    content += "Nome:" + usuario.getNome() + "\n";
-                    content += "Email:" + usuario.getEmail() + "\n";
+                    content += usuario.toString();
                     textView.append(content);
                 }
             }
@@ -56,4 +69,5 @@ public class Tela2 extends AppCompatActivity {
         });
 
     }
+
 }
--- app/src/main/java/com/example/app_desenvolvimento_android/Usuarios.java
+++ app/src/main/java/com/example/app_desenvolvimento_android/Usuarios.java
@@ -2,17 +2,23 @@ package com.example.app_desenvolvimento_android;
 
 public class Usuarios {
     private int id;
-    private String nome;
+    private String name;
     private String email;
 
     public int getId() {
         return id;
     }
     public String getNome() {
-        return nome;
+        return name;
     }
     public String getEmail() {
         return email;
     }
 
+    @Override
+    public String toString() {
+        return  "ID: " + id + "\n" +
+                "Nome: " + name + "\n" +
+                "Email: " + email + "\n\n";
+    }
 }
